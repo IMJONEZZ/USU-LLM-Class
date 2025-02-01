4eababe2becb8d9bc8f66303a237ccde0f02ea06
eliasthompson304 <A02392122@usu.edu>
2025-01-31 23:30:11 -0700

    ran test to check if batch sizes are returning correctly

--- src/test_dataloader.py
+++ src/test_dataloader.py
@@ -11,9 +11,19 @@ def sample_dataset():
     return TensorDataset(inputs, labels)
 
 def test_dataloader_initialization(sample_dataset):
-    #this tests if the dataloader initializes properly
+    #this tests if the dataloader initializes properly -> PASSED
     dataloader = CustomDataLoader(sample_dataset, batch_size=2, shuffle=False)
     assert len(dataloader) == 5 #10 samples with batch_size 2
 
+def test_dataloader_batch_size(sample_dataset):
+    #tests if dataloader makes correct batch size
+    batch_size = 3
+    dataloader = CustomDataLoader(sample_dataset, batch_size=batch_size, shuffle=False)
+    batches = list(dataloader)
+
+    assert len(batches) == (len(sample_dataset) + batch_size - 1) // batch_size
+    for batch in batches[:-1]:
+        assert batch[0].shape[0] == batch_size
+        assert batch[1].shape[0] == batch_size
 if __name__ == "__main__":
     pytest.main()
