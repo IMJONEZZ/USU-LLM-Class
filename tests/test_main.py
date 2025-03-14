4cbd71e3797eb1b78867d783c8d00b9f0dd5be3b
“First <frankchenhaozhe@gmail.com>
2025-03-14 09:58:27 -0600

    Homework assignment 7 initial commit

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,7 +3,7 @@ from main import hello_world
 
 
 def test_hello_world():
-    assert hello_world("World") == "Hello, World!"
+    assert hello_world("World") == "Hello, World!!"
     assert hello_world("Bob") == "Hello, Bob!"
     assert hello_world("") == "Hello, !"
     assert hello_world(123) == "Hello, 123!"
