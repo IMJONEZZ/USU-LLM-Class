fd49d1a08e6e504a367f9a6f0020ca7a8fdd5225
Chazzwazzler <erickgraham4@gmail.com>
2025-04-04 19:49:34 -0600

    programmed

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
-def hello_world(name: str) -> str:
-    return f"Hello, {name}!"
- 
+import dspy
+from dspy import Tool;
+import wikipedia
+
+MAX_SENTENCES = 1000;
+
+lm = dspy.LM('ollama_chat/erg', api_base='http://localhost:11434', api_key='')
+dspy.configure(lm=lm)
+
+def search_wikipedia(query: str):
+    summary = wikipedia.summary(query)
+    return summary
+
+rag = dspy.ChainOfThought('context, question -> response')
+
+question=input("Enter your question: ")
+
+response = rag(context=search_wikipedia(question), question=question)
+
+print(response.completions.response)
