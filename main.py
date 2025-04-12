5b15d1ed080eee7db8248491ee57fcbe0a966767
Erick Graham <a02414735@aggies.usu.edu>
2025-04-11 21:40:02 -0600

    programmed

--- main.py
+++ main.py
@@ -1,2 +1,39 @@
-def hello_world(name: str) -> str:
-    return f"Hello, {name}!"
+from ollama import chat
+import streamlit as st
+import time
+
+def get_ollama_response(user_input):
+    model_name = "llama-3.2"
+
+    try:
+        response = chat(model=model_name, messages=[{'role': 'user', 'content': user_input}])
+        return response['message']['content']
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
+
+st.title("Simple chat")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("What is up?"):
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+# Display assistant response in chat message container
+with st.chat_message("assistant"):
+    response = st.write(get_ollama_response(prompt))
+# Add assistant response to chat history
+st.session_state.messages.append({"role": "assistant", "content": response})
+
