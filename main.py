c9e5ca269e304a26ffab249227bbaef84536bfb5
Chazzwazzler <erickgraham4@gmail.com>
2025-04-25 21:19:57 -0600

    formatted

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 from zenml import pipeline, step
 
+
 class StarWarsDataset(Dataset):
     def __init__(self, file):
         with open(file, "r") as f:
@@ -14,7 +15,7 @@ class StarWarsDataset(Dataset):
     def __getitem__(self, i):
         item = self.data[i]
         return item
-    
+
 
 @step
 def dataloader(path) -> DataLoader:
@@ -22,9 +23,11 @@ def dataloader(path) -> DataLoader:
     dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
     return dataloader
 
+
 @pipeline
 def pipeline():
     dataloader("SW_EpisodeIV_VI.json")
+
 
 if __name__ == "__main__":
     run = pipeline()
--- tests/test_main.py
+++ tests/test_main.py
@@ -5,8 +5,15 @@ from torch.utils.data import DataLoader
 def test_star_wars_dataset():
     dataset = StarWarsDataset("SW_EpisodeIV_VI.json")
     assert len(dataset) == 84  # Verify the dataset length matches the JSON file
-    assert dataset[0]["Line"] == "Did you hear that?  They've shut down the main reactor.  We'll be destroyed for sure.  This is madness!"
-    assert dataset[83]["Line"] == "Quiet down will ya!  You got a mouth bigger than a meteor crater!"
+    assert (
+        dataset[0]["Line"]
+        == "Did you hear that?  They've shut down the main reactor.  We'll be destroyed for sure.  This is madness!"
+    )
+    assert (
+        dataset[83]["Line"]
+        == "Quiet down will ya!  You got a mouth bigger than a meteor crater!"
+    )
+
 
 def test_dataloader():
     loader = dataloader("SW_EpisodeIV_VI.json")
